260fad75471f2922d12716f96768cd8f66747ede
Oladimeji <oladimejiolawale1999@gmail.com>
2025-11-12 07:32:04 -0800

    Add client folder to main repo

---
+++ client/src/redux/store.js
@@ -0,0 +1,27 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import userReducer from './slices/userSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+        serializableCheck: false
+    })
+})
+
+export const persistor = persistStore(store)
