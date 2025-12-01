4f87b0aafd3b76e0f485ba20c4a985d28e1ad27e
Oladimeji <oladimejiolawale1999@gmail.com>
2025-11-30 22:40:28 -0800

    create search page ui

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/search" element={<Search />} />
       <Route path="/listings/:listingId" element={<Listing />} />
       <Route element = {<PrivateRoute/>}>
       <Route path="/create-listing" element={<CreateListing />} />
